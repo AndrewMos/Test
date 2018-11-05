9a4d355847df56c9cff332e7f201ad09c2e0b73f
Andrii <andmos@st.amu.edu.pl>
2018-11-05 15:19:10 +0100

    First version

---
+++ src/main/java/Person.java
@@ -0,0 +1,13 @@
+public class Person {
+    String name;
+    String surname;
+    String pesel;
+    String city;
+
+    public Person(String name, String surname, String pesel, String city) {
+        this.name = name;
+        this.surname = surname;
+        this.pesel = pesel;
+        this.city = city;
+    }
+}
